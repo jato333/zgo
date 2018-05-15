e9f46a1e0279d570c8aa19f2b982fa30b59d7833
jato333 <yubodou333@126.com>
2018-05-15 22:32:40 +0800

    change
    change

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"net/http"
+
 	l4g "github.com/alecthomas/log4go"
 )
 
 func main() {
 	l4g.LoadConfiguration("config/log4go.xml")
-	l4g.Info("Hello,word!")
 	defer l4g.Close()
+
+	router := NewRouter()
+
+	l4g.Info(http.ListenAndServe(":8080", router))
 }
