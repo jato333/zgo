24d9c5e35d0f82775fa2577720cf82eb969192e5
jato333 <yubodou333@126.com>
2018-05-16 21:29:16 +0800

    fix bugs
    fix bugs

--- main.go
+++ main.go
@@ -28,20 +28,12 @@ func main() {
 		log4go.Error("无法加载ini配置文件：%s", err)
 	}
 
-	//	config, err := yaml.ReadFile("conf.yaml")
-	//	if err != nil {
-	//		log4go.Error("无法加载yaml配置文件：%s", err)
-	//	}
-	//	fmt.Println(config.Get("path"))
-
-	version := cfg.MustValue("server", "version", "1.0.0.0")
-
-	host := cfg.MustValue("server", "host", "127.0.0.1")
-	port := cfg.MustValue("server", "port", "8080")
-
-	readtimeout := cfg.MustInt("server", "readtimeout", 10)
-	writetimeout := cfg.MustInt("server", "writetimeout", 10)
-	maxheaderbytes := cfg.MustInt("server", "maxheaderbytes", 20)
+	version := cfg.MustValue("Server", "version", "1.0.0.0")
+	host := cfg.MustValue("Server", "host", "127.0.0.1")
+	port := cfg.MustValue("Server", "port", "8080")
+	readtimeout := cfg.MustInt("Server", "readtimeout", 10)
+	writetimeout := cfg.MustInt("Server", "writetimeout", 10)
+	maxheaderbytes := cfg.MustInt("Server", "maxheaderbytes", 20)
 
 	log4go.Info("Server Zgo "+version+" starting at [%s", host+":"+port+"]\n"+banner)
 
